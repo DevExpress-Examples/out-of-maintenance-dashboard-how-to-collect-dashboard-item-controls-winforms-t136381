3c0b51ff20d1caadd8c5479eb9f77db4f53ef490
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/NewProject/Form1.cs
@@ -0,0 +1,44 @@
+﻿using DevExpress.DashboardWin.Native;
+using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NewProject
+{
+    public partial class Form1 : XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dashboardViewer1_DataLoading(object sender, DevExpress.DataAccess.DataLoadingEventArgs e)
+        {
+            List<MyClass> list = CreateDataSource();
+            e.Data = list;
+        }
+        private static List<MyClass> CreateDataSource()
+        {
+            List<MyClass> list = new List<MyClass>(10);
+            Random _r = new Random();
+            for (int i = 0; i < list.Capacity; i++)
+                list.Add(new MyClass() { ID = i, Name = "Name" + i, Sales = (float)_r.NextDouble(), Date = DateTime.Now.AddDays(i)});
+            return list;
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            IEnumerable<Control> controls = ((IUnderlyingControlProvider)dashboardViewer1).GetUnderlyingControls();
+            PrintControlInfo(controls);
+        }
+        private static void PrintControlInfo(IEnumerable<Control> controls)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (Control control in controls)
+                stringBuilder.AppendLine(control.GetType().ToString());
+            XtraMessageBox.Show(stringBuilder.ToString());
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Dashboard - How to collect dashboard item controls (Winforms)
+
+
+Starting with version 14.2 this approach is obsolete. See the <a href="https://www.devexpress.com/Support/Center/p/Q464098">Grid Dashboard Item - Provide a way to save column width in the grid item</a> thread for more details.
+
+<br/>
+
+
